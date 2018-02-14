fdb3183ab30ca673020802d40c05f889070d45c4
Gaurav Munjal <gaurav@vestorly.com>
2018-02-13 19:07:53 -0500

    Add computed property macros

---
+++ addon/cp-macros.js
@@ -0,0 +1,32 @@
+import { get } from '@ember/object';
+import computed from 'ember-macro-helpers/computed';
+import BufferedProxy from 'ember-buffered-proxy/proxy';
+import BufferedArrayProxy from 'ember-buffered-array-proxy/proxy';
+import BufferedArrayPromiseProxy from './proxy';
+
+export function buffered(property) {
+  return computed(property, function() {
+    let content = get(this, property);
+    if (content) {
+      return BufferedProxy.create({ content });
+    }
+  });
+}
+
+export function bufferedArray(property) {
+  return computed(property, function() {
+    let content = get(this, property);
+    if (content) {
+      return BufferedArrayProxy.create({ content });
+    }
+  });
+}
+
+export function bufferedPromiseArray(property) {
+  return computed(property, function() {
+    let content = get(this, property);
+    if (content) {
+      return BufferedArrayPromiseProxy.create({ content });
+    }
+  });
+}
